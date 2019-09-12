c29b0dae1a9d53fc415d2ca482a99e915e029065
PangDu <xian312117@gmail.com>
2019-09-12 15:54:32 +0800

    delete files

--- AFOGitHub/AFOGitHub.h
+++ AFOGitHub/AFOGitHub.h
@@ -15,7 +15,6 @@ FOUNDATION_EXPORT double AFOGitHubVersionNumber;
 FOUNDATION_EXPORT const unsigned char AFOGitHubVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <AFOGitHub/PublicHeader.h>
-#import <AFOGitHub/Masonry.h>
 #import <AFOGitHub/Aspects.h>
 #import <AFOGitHub/SVPullToRefresh.h>
 #import <AFOGitHub/UIImageView+WebCache.h>
